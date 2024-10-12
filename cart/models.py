dc8f21302518d863898c1ac7095dbf9b0cc04081
DaniyalKhan <daniyalkhand16@gmail.com>
2024-10-12 17:16:54 +0500

    Removed the field total_prod_qtys from the model Cart. Added the field created_at to the model Cart. Added the field created_at to the model Cart Item. Added the urls.py to the cart app.

--- cart/models.py
+++ cart/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from users.models import CustomUser
 from products.models import Product
@@ -6,8 +7,8 @@ from products.models import Product
 
 class Cart(models.Model):
     buyer_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
-    total_prod_qtys = models.PositiveIntegerField()
     total_price = models.DecimalField(max_digits=14, decimal_places=3)
+    created_at = models.DateTimeField(default=timezone.now)
 
 
 class CartItem(models.Model):
@@ -15,3 +16,4 @@ class CartItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField()
     price = models.DecimalField(max_digits=14, decimal_places=3)
+    created_at = models.DateTimeField(default=timezone.now)
