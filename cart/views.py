4815918b71e0527b71efb46d20bf2e1af54fcca8
DaniyalKhan <daniyalkhand16@gmail.com>
2024-11-10 22:37:51 +0500

    Created the add to cart view refactored the code for add to cart button in templates Did some minor changes in the Cart and Cartitem model.

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+from .models import Cart, CartItem
+from products.models import Product
+from utils.decorators import is_buyer
+
+
+@login_required(login_url='/buyer/login/')
+@is_buyer
+def AddProductToCart(request, product_id, product_qty):
+    product = get_object_or_404(Product, id=product_id)
+    price = product.sales_price * product_qty
+    cart, cart_created = Cart.objects.get_or_create(buyer_id=request.user)
+    cart_item, cart_item_created = CartItem.objects.get_or_create(cart=cart, product=product)
+    cart_item.quantity += product_qty
+    cart_item.price += price
+    cart_item.save()
+    cart.total_price += price
+    cart.save()
+    previous_url = request.META.get('HTTP_REFERER', '/')
+    return HttpResponseRedirect(previous_url)
